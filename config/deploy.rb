b57565e017caf98bf58505986929c8387082e9dd
Catalin Boruga <catalinboruga@yahoo.com>
2015-11-11 23:30:38 +0100

    Add for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'cata_test'
+set :application, 'cata-test'
 set :repo_url, 'git@github.com:bosvrum/cata-test.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/cata_test'
+set :deploy_to, '/home/cata/cata-test'
 
 # Default value for :scm is :git
 # set :scm, :git
